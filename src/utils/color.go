fa2962d2aa1c4e9e2dfa0af2d03c6d02732baada
EndIsNear <alexanderdespotov@gmail.com>
2014-12-19 23:33:14 +0200

    Fixed ToRGB and Devide.

--- src/utils/color.go
+++ src/utils/color.go
@@ -6,8 +6,10 @@ func NewColor(r, g, b uint8) color {
 	return color{float64(r) / 255, float64(g) / 255, float64(b) / 255}
 }
 
-func (c *color) ToRGB() (uint8, uint8, uint8) {
-
+func (c color) ToRGB() (uint8, uint8, uint8) {
+	c[0] = between01(c[0])
+	c[1] = between01(c[1])
+	c[2] = between01(c[2])
 	return uint8(c[0] * 255), uint8(c[1] * 255), uint8(c[2] * 255)
 }
 
@@ -32,5 +34,15 @@ func ColorFloatMulti(c color, mult float64) color {
 }
 
 func ColorFloatDevide(c color, divider float64) color {
-	return color{c[0] * divider, c[1] * divider, c[2] * divider}
+	return color{c[0] / divider, c[1] / divider, c[2] / divider}
+}
+
+func between01(color float64) float64 {
+	if color < 0 {
+		color = 0
+	}
+	if color > 1 {
+		color = 1
+	}
+	return color
 }
