5e6bad8612d128915c91c2202b36bd03e8eaeb41
EndIsNear <alexanderdespotov@gmail.com>
2015-01-10 11:23:15 +0200

    Made color visible out of pack.

--- src/utils/color.go
+++ src/utils/color.go
@@ -1,48 +1,48 @@
 package utils
 
-type color [3]float64
+type Color [3]float64
 
-func NewColor(r, g, b uint8) color {
-	return color{float64(r) / 255, float64(g) / 255, float64(b) / 255}
+func NewColor(r, g, b uint8) Color {
+	return Color{float64(r) / 255, float64(g) / 255, float64(b) / 255}
 }
 
-func (c color) ToRGB() (uint8, uint8, uint8) {
+func (c Color) ToRGB() (uint8, uint8, uint8) {
 	c[0] = between01(c[0])
 	c[1] = between01(c[1])
 	c[2] = between01(c[2])
 	return uint8(c[0] * 255), uint8(c[1] * 255), uint8(c[2] * 255)
 }
 
-func (c *color) Multiply(mult float64) {
+func (c *Color) Multiply(mult float64) {
 	*c = ColorFloatMulti(*c, mult)
 }
 
-func (c *color) Devide(divider float64) {
+func (c *Color) Devide(divider float64) {
 	*c = ColorFloatDevide(*c, divider)
 }
 
-func ColorAddition(left, right color) color {
-	return color{left[0] + right[0], left[1] + right[1], left[2] + right[2]}
+func ColorAddition(left, right Color) Color {
+	return Color{left[0] + right[0], left[1] + right[1], left[2] + right[2]}
 }
 
-func ColorMultiplication(left, right color) color {
-	return color{left[0] * right[0], left[1] * right[1], left[2] * right[2]}
+func ColorMultiplication(left, right Color) Color {
+	return Color{left[0] * right[0], left[1] * right[1], left[2] * right[2]}
 }
 
-func ColorFloatMulti(c color, mult float64) color {
-	return color{c[0] * mult, c[1] * mult, c[2] * mult}
+func ColorFloatMulti(c Color, mult float64) Color {
+	return Color{c[0] * mult, c[1] * mult, c[2] * mult}
 }
 
-func ColorFloatDevide(c color, divider float64) color {
-	return color{c[0] / divider, c[1] / divider, c[2] / divider}
+func ColorFloatDevide(c Color, divider float64) Color {
+	return Color{c[0] / divider, c[1] / divider, c[2] / divider}
 }
 
-func between01(color float64) float64 {
-	if color < 0 {
-		color = 0
+func between01(Color float64) float64 {
+	if Color < 0 {
+		Color = 0
 	}
-	if color > 1 {
-		color = 1
+	if Color > 1 {
+		Color = 1
 	}
-	return color
+	return Color
 }
