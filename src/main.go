a119066ccc267218d3d2d3cc97cabc0dbd97f5ac
EndIsNear <alexanderdespotov@gmail.com>
2015-01-15 17:35:02 +0200

    Reworked main to parse command line args.

--- src/main.go
+++ src/main.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"Go-Raytracer/src/raytracer"
+	"fmt"
+	"os"
 )
 
-var winWidth, winHeight uint16 = 800, 600
+const (
+	help = `
+-s  "filepath" read scene from filepath
+-d - display rendering
+-o "filepath" save image to filepath`
+	tooFewArg                  = `Too few arguments.`
+	errParseArg                = `Can't parse args.`
+	winWidth, winHeight uint16 = 800, 600
+)
 
 func main() {
-	ok, render := raytracer.NewRenderManager(winWidth, winHeight)
-	if !ok {
-		return
+	sceneFilepath := ""
+	outputFilepath := ""
+	displayRendering := false
+	saveImage := false
+	argc := len(os.Args)
+	if argc <= 1 {
+		fmt.Println(tooFewArg)
+		fmt.Println(help)
 	}
-	render.InitScene()
+
+	for i := 1; i < argc; i++ {
+		if os.Args[i] == "-d" {
+			displayRendering = true
+		} else if os.Args[i] == "-o" {
+			saveImage = true
+			i++
+			outputFilepath = os.Args[i]
+		} else if os.Args[i] == "-s" {
+			i++
+			sceneFilepath = os.Args[i]
+		} else {
+			fmt.Println(errParseArg)
+			fmt.Printf("Can't parse %s .\n", os.Args[i])
+			fmt.Println(help)
+		}
+	}
+
+	render := raytracer.NewRenderManager(winWidth, winHeight, 0)
+	render.InitScene(sceneFilepath)
 	render.StartRendering()
-	render.Display()
-	render.Destroy()
+
+	if displayRendering {
+		for render.GetState() == raytracer.RENDERING {
+			//display progress
+		}
+	}
+
+	if saveImage {
+		//save the result
+		outputFilepath = outputFilepath
+	}
 }
